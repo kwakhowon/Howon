98bd84c1706280efb3216831bbf442a70be1ef44
kwakhowon <howon0206@naver.com>
2019-04-05 11:29:15 +0900

    0405

--- Web/WebClientLabs/practice/WebContent/index.js
+++ Web/WebClientLabs/practice/WebContent/index.js
@@ -1,40 +1,21 @@
-console.log('Im Working. Im JS. Im Beautiful. Imworth it');
-const mon = "Mon";
-const tue = "Tue";
-const wed = "Wed";
-const thu = "Thu";
-const fri = "Fri";
+const title = document.querySelector("#title");
 
-const dayOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" , true , 26];
-console.log(dayOfWeek[2]);
+const CLICKED_CLASS = "clicked";
 
-const userInfo = ["howon","26", true, "seoul"];
+function handleClick(){ 
+	// const hasClass = title.classList.contains(CLICKED_CLASS);
+	// if(!hasClass){
+	// 	title.classList.add(CLICKED_CLASS);
+	// }else{
+	// 	title.classList.remove(CLICKED_CLASS);
 
-console.log(userInfo[0]);
+	// }
 
-const howon = {
-		name : "howon",
-		age : 26,
-		gender : "male",
-		isHandsome:false,
-		favMovies:["Lotr", "oldboy", "mine"],
-		favFood : [ {name : "kimchi", fatty:false}, 
-		{name : "cheese burger", fatty : true }]
+	title.classList.toggle(CLICKED_CLASS);
 }
 
-function sayHello(name, age){
-	return `Hello", ${name} you are ${age} years old`;
-}
 
-console.log(howon.favFood[0].fatty, howon.favMovies[0]);
-sayHello("howon",15);
-
-const greetHowon = sayHello("howon", 15)
-console.log(greetHowon)
-const calculator = {
-	plus : function(a,b){
-		return a+b;
-	}
+function init(){
+	title.addEventListener("click", handleClick);
 }
-const plus = calculator.plus(5,5)
-console.log(plus)
+init();
